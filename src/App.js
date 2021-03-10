1b2308d5e8f9ba5b57ceb5d2b461f3a36f537409
FeED IT <csefelipeedu@gmail.com>
2021-03-09 21:58:32 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, {useState} from 'react'
 import { ReactComponent as Robot } from '../src/images/robot.svg'
 import './App.css'
 import Carregando from '../src/images/Spinner-1s-200px.svg'
+import { MdClear, MdMoveToInbox } from "react-icons/md";
+
 //JSX Java script eXtension
 //Hook - useState
 function App(){
   const [pessoas, setPessoas] = useState([])
   const [carregando, setCarregando] = useState(false)
+  const [idade, setIdade] = useState('')
+  const [etnia, setEtnia] = useState('')
+  // const estiloIcone = {} estilos para o icone
 
   function ListaPessoas(){
     const listagemPessoas = pessoas.map((pessoa)=>
@@ -20,7 +25,12 @@ function App(){
 
   async function obterFoto(){
     setCarregando(true)
-    let url= "https://api.generated.photos/api/v1/faces?api_key=d-JBW1-p-pw924AbITR2Qw"
+    let chaveAPI = process.env.REACT_APP_APIKEY
+    const filtraEtnia = etnia.length > 0 ? `&ethnicity=${etnia}` : ''
+    const filtraIdade = idade.length > 0 ? `&age=${idade}` : ''
+
+    let url= 
+`https://api.generated.photos/api/v1/faces?api_key=${chaveAPI}${filtraEtnia}${filtraIdade}&order_by=random`
     await fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -49,10 +59,41 @@ function App(){
 
     </div>
    
+   <div className="linha">
+      <label>Idade:</label>
+      <select onChange = {event => setIdade(event.target.value)}>
+      <option value="">Todas</option>
+      <option value="infant">infantil</option>
+      <option value="child">Criança</option>
+      <option value="young-adult">Jovem</option>
+      <option value="adult">Adulto</option>
+      <option value="elderly">Idoso</option>
+      </select>
+    <label>Etnia:</label>
+    <select onChange = {e => setEtnia(e.target.value)}> 
+    <option value="">Todas</option>
+      <option value="white">Branca</option>
+      <option value="latino">Latina</option>
+      <option value="asian">Asiática</option>
+      <option value="black">Negra</option>
+    </select>
+
+   </div>
+
+
+  <div className="linha"></div>
     <button type='button' onClick={obterFoto}>
 
-         OBTER IMAGENS
+         <MdMoveToInbox/> OBTER IMAGENS
     </button>
+    {pessoas.length > 0 &&
+    <button type ='button' onClick={() => setPessoas ([])}>
+      <MdClear/> Limpar imagens
+      {/* <MdClear style  {estiloIcone} />  */}
+    </button>
+    }
+    
+  
    
   </div>
   )
